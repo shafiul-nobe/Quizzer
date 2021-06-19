2b747bbf2637f7d31bd17eb7600f6df5adbdde19
shafiulnobe <shafiul.nobe@selise.ch>
2021-06-20 01:28:56 +0600

    reorderd files

---
+++ Quizzer.Queries/QueryHandlers/Question/QuestionByIdQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Quizzer.Queries.Contracts;
+using Quizzer.Queries.Models;
+using Quizzer.Queries.Models.Question;
+using Quizzer.Queries.Queries.Question;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Quizzer.Queries.QueryHandlers.Question
+{
+    public class QuestionByIdQueryHandler : IRequestHandler<QuestionByIdQuery, QueryResponse<QuestionByIdQueryResponseModel>>
+    {
+        private readonly IQuestionService questionService;
+
+        public QuestionByIdQueryHandler(IQuestionService questionService)
+        {
+            this.questionService = questionService;
+        }
+
+        public async Task<QueryResponse<QuestionByIdQueryResponseModel>> Handle(QuestionByIdQuery query, CancellationToken cancellationToken)
+        {
+            var question = questionService.GetQuestionById(query);
+
+            return QueryResponse<QuestionByIdQueryResponseModel>.Success(question);
+        }
+
+       
+    }
+}
